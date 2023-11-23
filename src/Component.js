4e6ba3efabcb794a37a04be77a20fd4b611b4cc3
Priyanka <priyapasupuleti23@gmail.com>
2023-11-23 19:32:16 +0530

    Added NewComponent and integrated it into App.js

---
+++ src/Component.js
@@ -0,0 +1,14 @@
+// src/components/NewComponent.js
+
+import React from 'react';
+
+const NewComponent = () => {
+  return (
+    <div>
+      <h2>New Component</h2>
+      {/* Your component content goes here */}
+    </div>
+  );
+};
+
+export default NewComponent;
